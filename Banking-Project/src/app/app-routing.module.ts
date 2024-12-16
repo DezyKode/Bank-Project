18cf4151e7a7b1af0d0d0623cddf25e1a8dfe005
ashanka710 <ashankad343@gmail.com>
2024-12-16 18:08:12 +0530

    login done maneger and admin

--- Banking-Project/src/app/app-routing.module.ts
+++ Banking-Project/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './components/Admin/login/login.component';
+import { ManagerLoginComponent } from './components/Maneger/maneger-login/manager-login.component';
+// import { ManagerLoginComponent } from './components/Maneger/manager-login.component';
+// import { LoginComponent } from './components/Manager/login/login.component';
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'managerlogin',component:ManagerLoginComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
