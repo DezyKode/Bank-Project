5f9b6b1f343afd8d90806b449435edfa1c2c792a
shubham kate <shubhamkate962@gmail.com>
2024-12-17 15:24:17 +0530

    Registration

--- Banking-Project/src/app/app-routing.module.ts
+++ Banking-Project/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/Admin/login/login.component';
 import { ManagerLoginComponent } from './components/Maneger/maneger-login/manager-login.component';
-
+import { RegistrationComponent } from './components/Employee/registration/registration.component';
 const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'', redirectTo:'login', pathMatch:'full'},
-  {path:'managerlogin',component:ManagerLoginComponent}
+  {path:'managerlogin',component:ManagerLoginComponent},
+  {path:'registration',component:RegistrationComponent}
 ]
 
 @NgModule({
