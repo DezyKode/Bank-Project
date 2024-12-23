19acec6add9b9ba7da54d609d7f4399b376bfa60
ashanka710 <ashankad343@gmail.com>
2024-12-23 16:32:26 +0530

    done

--- Banking-Project/src/app/app.module.ts
+++ Banking-Project/src/app/app.module.ts
@@ -6,13 +6,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/Admin/login/login.component';
 import { ManagerLoginComponent } from './components/Maneger/maneger-login/manager-login.component';
 import { RegistrationComponent } from './components/Employee/registration/registration.component';
-import { leadComponent } from './components/lead/LeadDashboard/lead.component';
+// import { leadComponent } from './components/lead/LeadDashboard/lead.component';
 import { CheckeligibilityComponent } from './components/checkeligibility/checkeligibility.component';
 import { PersonalloanComponent } from './components/personalloan/personalloan.component';
 import { NewtaskComponent } from './components/newtask/newtask.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { CardComponent } from './components/lead/card/card.component';
+// import { CardComponent } from './components/lead/card/card.component';
+import { LeadComponent } from './components/lead/LeadDashboard/lead.component';
 
 @NgModule({
   declarations: [
@@ -21,9 +22,10 @@ import { CardComponent } from './components/lead/card/card.component';
     ManagerLoginComponent,
     RegistrationComponent,
     NewtaskComponent,
-    leadComponent,
+    LeadComponent,
     CheckeligibilityComponent,
     PersonalloanComponent,
+    LeadComponent,
   ],
   imports: [
     BrowserModule,
