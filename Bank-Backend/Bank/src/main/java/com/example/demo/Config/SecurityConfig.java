eb4dab37fc5539935d16269cc0973d064fb713ec
Jaydev Solkar <jpsolkar@email.com>
2024-12-23 10:49:02 +0530

    done

---
+++ Bank-Backend/Bank/src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection
+                .authorizeHttpRequests(authorizeRequests ->
+                                authorizeRequests
+                                        .requestMatchers("/addemployee/**" ).permitAll()
+                                        .requestMatchers("/elegibility/**").permitAll()// Public POST requests
+
+                                        .anyRequest().authenticated() // Authenticate all other requests
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Use stateless session management
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- Bank-Backend/Bank/src/main/java/com/example/demo/Service/AddEmployeeServ.java
+++ Bank-Backend/Bank/src/main/java/com/example/demo/Service/AddEmployeeServ.java
@@ -1,6 +1,7 @@
 package com.example.demo.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.Entity.AddEmployee;
@@ -12,8 +13,13 @@ public class AddEmployeeServ {
 	@Autowired
 	private AddEmployeeRepo addEmployeeRepo;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	
 	public AddEmployee addEmployee(AddEmployee employee)
 	{
+		employee.setPassward(passwordEncoder.encode(employee.getPassward()));
 		return addEmployeeRepo.save(employee);
 	}
 }
