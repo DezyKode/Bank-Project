585df7ea9ea5ffc6feaa8ec7a52ad5d50f0e4f43
ashanka710 <ashankad343@gmail.com>
2025-01-20 18:59:49 +0530

    done

--- Banking-Project/src/app/components/masters/edit-document/edit-document.component.ts
+++ Banking-Project/src/app/components/masters/edit-document/edit-document.component.ts
@@ -1,81 +1,40 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-edit-document',
   templateUrl: './edit-document.component.html',
-  styleUrls: ['./edit-document.component.css'],
+  styleUrls: ['./edit-document.component.css']
 })
 export class EditDocumentComponent {
-  @Output() documentSaved = new EventEmitter<any>();
-
-  document: any = {
+  document = {
     typeOfLoan: '',
-    employmentType: '',
-    documents: [],
-    file: null
+    employeeType: ''
   };
 
-  addItem(): void {
-    this.document.documents.push('');
-  }
+  loanCriteriaRows = [
+    { cibilScoreStart: '', cibilScoreEnd: '', loanRequirementStart: '', loanRequirementEnd: '' }
+  ];
 
-  removeItem(index: number): void {
-    this.document.documents.splice(index, 1);
-  }
+  isEditMode = false;
 
-  onFileSelected(event: any): void {
-    const file = event.target.files[0];
-    if (file) {
-      this.document.file = file;
+  // Update Document method
+  updateDocument() {
+    if (this.isEditMode) {
+      // Logic for updating the document
+      console.log('Updating document with data:', this.document);
+    } else {
+      // Logic for creating a new document
+      console.log('Creating a new document with data:', this.document);
     }
   }
 
-  saveChanges(): void {
-    // Emit the document data to the parent component
-    this.documentSaved.emit(this.document);
-    console.log('Document saved:', this.document);
+  // Add a new row to the loanCriteriaRows
+  addRow() {
+    this.loanCriteriaRows.push({ cibilScoreStart: '', cibilScoreEnd: '', loanRequirementStart: '', loanRequirementEnd: '' });
   }
 
-  goBack(): void {
-    window.history.back(); // Or use Angular's Location service
+  // Delete a row from loanCriteriaRows
+  deleteRow(index: number) {
+    this.loanCriteriaRows.splice(index, 1);
   }
-
-
-
-// Array to store the table rows data
-loanCriteriaRows = [
-  {
-    cibilScoreStart: '',
-    cibilScoreEnd: '',
-    loanRequirementStart: '',
-    loanRequirementEnd: '',
-   /*  salariedROI: '',
-    nonSalariedROI: '' */
-  }
-];
- 
-// Function to add a new row to the table
-addRow() {
-  this.loanCriteriaRows.push({
-    cibilScoreStart: '',
-    cibilScoreEnd: '',
-    loanRequirementStart: '',
-    loanRequirementEnd: '',
-    /* salariedROI: '',
-    nonSalariedROI: '' */
-  });
 }
- 
-// Function to delete a row
-deleteRow(index: number) {
-  this.loanCriteriaRows.splice(index, 1);
-}
- 
-// Function to save data (you can implement your own logic here)
-saveRow(index: number) {
-  const rowData = this.loanCriteriaRows[index];
-  console.log("Saved Data: ", rowData);
-  // You can send this data to a backend or save it locally as needed
-}
-}
- 
